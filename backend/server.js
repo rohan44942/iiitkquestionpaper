df52e3ff0253da6203c9ca6ed32962983b9a7983
rohan <rohannooniwal39@gmail.com>
2024-10-07 16:17:24 +0530

    notes section added admin section working started and its little unstable update

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,14 @@ const GridFsStorage = require("multer-gridfs-storage").GridFsStorage;
 const mongoose = require("mongoose");
 const { connect } = require("../backend/connectdb/connectdb");
 const path = require("path");
+const cloudinary = require("cloudinary").v2;
+const fileUpload = require("express-fileupload");
+const User = require("Auth0");
 require("dotenv").config();
-// const mongoapi = process.env.MONGO_URL_ATLASH;
-const mongoapi =
+// const mongoapi_rohan = process.env.MONGO_URL_ATLASH;
+const mongoapi_rohan =
   "mongodb+srv://2021kucp1109:OkfIRMFSZpnuv1UI@cluster0.4brou.mongodb.net/iiitk_resources?retryWrites=true&w=majority&appName=Cluster0";
+const mongoapi_praveen = "";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,22 +20,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true }));
 
 // Initialize gfs and gridfsBucket
 let gfs, gridfsBucket;
 
 connect().then(() => {
-  console.log(mongoapi);
   const db = mongoose.connection.db;
   gridfsBucket = new mongoose.mongo.GridFSBucket(db, { bucketName: "uploads" });
   gfs = gridfsBucket;
   // console.log("thiss is runing")
 });
 
-// Set up GridFS storage engine
 const storage = new GridFsStorage({
-  // url: "mongodb+srv://2021kucp1109:OkfIRMFSZpnuv1UI@cluster0.4brou.mongodb.net/iiitk_resources?retryWrites=true&w=majority&appName=Cluster0",
-  url: mongoapi,
+  
+  url: mongoapi_rohan,
   file: (req, file) => {
     const filename = `${file.fieldname}_${Date.now()}${path.extname(
       file.originalname
@@ -110,6 +113,73 @@ app.get("/api/uploads", async (req, res) => {
     res.json(files);
   } catch (err) {
     res.status(500).json({ message: "Error fetching files", error: err });
+  }
+});
+
+// uploading the notes pdfs all implementation here
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || dlmypwh71,
+  api_key: process.env.CLOUDINARY_API_KEY || 142613153773972,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// creating the schema
+const noteSchema = new mongoose.Schema({
+  subjectName: String,
+  year: String,
+  semester: String,
+  branch: String,
+  fileLink: String,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+app.post("/api/upload", (req, res) => {
+  const file = req.files.file;
+
+  cloudinary.uploader.upload(file.tempFilePath, (error, result) => {
+    if (error) {
+      console.error("Error uploading to Cloudinary:", error);
+      return res.status(500).send("Cloudinary upload failed.");
+    }
+
+    return res.json({ secure_url: result.secure_url });
+  });
+});
+
+// Error handling middleware to capture errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.post("/api/upload/notes", async (req, res) => {
+  const { subjectName, year, semester, branch, fileLink } = req.body;
+
+  try {
+    const newNote = new Note({
+      subjectName,
+      year,
+      semester,
+      branch,
+      fileLink,
+    });
+
+    await newNote.save();
+    res.status(200).json({ message: "Note uploaded successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "Error saving note", error });
+  }
+});
+
+// get from the notes data base
+app.get("/api/upload/notes", async (req, res) => {
+  try {
+    const notes = await Note.find(); // Fetch all notes
+    res.json(notes); // Send notes as JSON
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
   }
 });
 
---
+++ my-react-app/src/Pages/Admin.js
@@ -0,0 +1,15 @@
+import LogoutButton from "../component/LogoutButton";
+import Profile from "../component/Profile";
+
+function Admin() {
+  return (
+    <div>
+      this is admin page
+      
+      <Profile />
+      <LogoutButton />
+    </div>
+  );
+}
+
+export default Admin;
