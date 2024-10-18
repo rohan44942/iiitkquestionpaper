98879747721dfbc7c977d89e08050b45e2105ca0
rohan <rohannooniwal39@gmail.com>
2024-10-18 15:20:05 +0530

    client id change

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Auth0Provider
     domain="dev-c7ap44czi23i7ka6.us.auth0.com"
-    // clientId="rfSZVd2Iw8bjiJ8NMYvN4ss4Mdn0FkLC" // iiitkresourcesv2
-    clientId="tNoLJgic8eDGLWCIyhHAkqiPaoT4XGUQ" // local host 3000
+    clientId="rfSZVd2Iw8bjiJ8NMYvN4ss4Mdn0FkLC" // iiitkresourcesv2
+    // clientId="tNoLJgic8eDGLWCIyhHAkqiPaoT4XGUQ" // local host 3000
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
